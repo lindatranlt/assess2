f685383300d80f2eccfcbc7e2b7637b4f98127b0
Linda Tran <133718039+lindatranlt@users.noreply.github.com>
2024-01-01 23:01:29 -0800

    orders done

--- orders.js
+++ orders.js
@@ -30,7 +30,20 @@
 */
 
 //CODE HERE
+class Ticket {
+    constructor(items, orderTime, customerID){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerID = customerID;
+        this.status = 'queued';
+    }
 
+    updateStatus(newStatus){
+        this.status = newStatus;
+        console.log("The order for customer ${this.customerID} is now ${this.status}");
+        // ^^ not sure why it's not inserting the values. It's just printing '${this.customerID}' and '${this.status}'
+    }
+}
 
 
 /*
@@ -45,7 +58,7 @@
 */
 
 //CODE HERE
-
+const firstTicket = new Ticket(['pizza', 'bread', 'soda'],'7:03 PM', 575);
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +67,4 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking');
